e970244dd90a2a0176f8f86274226ab6e30fe4b5
Abhishek Master <abhishekm.mobiiworld@gmail.com>
2019-09-05 13:00:07 +0530

    history changes

--- SampleFitBit/Source/ViewController/HistoryVC.swift
+++ SampleFitBit/Source/ViewController/HistoryVC.swift
@@ -10,28 +10,66 @@ import UIKit
 
 class HistoryVC: UIViewController {
 
-    @IBOutlet var startDateButton: UIButton!
-    @IBOutlet var endDateButton: UIButton!
-    
+    @IBOutlet var startDateField: UITextField!
+    @IBOutlet var endDateField: UITextField!
     @IBOutlet var totalStepLabel: UILabel!
+    var datePickerView = UIDatePicker()
+    let healthManager = HealthManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.setDatePicker()
     }
     
-    @IBAction func tappedStartButton(_ sender: Any) {
-        
+    func dateFromFormat(_ date: String) -> Date
+    {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+        let date = dateFormatter.date(from: date)
+        return date!
     }
     
-    @IBAction func tappedEndButton(_ sender: Any) {
-        
+    func setDatePicker() {
+        datePickerView.datePickerMode = .date
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
+        toolBar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.plain, target: self, action: #selector(handleDoneButton))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItem.Style.plain, target: self, action: #selector(handleCancelButton))
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        endDateField.inputView = datePickerView
+        endDateField.inputAccessoryView = toolBar
+        startDateField.inputView = datePickerView
+        startDateField.inputAccessoryView = toolBar
     }
     
+    @objc func handleDoneButton() {
+        if startDateField.isFirstResponder {
+            startDateField.text = String(describing : datePickerView.date)
+        } else {
+            endDateField.text = String(describing : datePickerView.date)
+        }
+        datePickerView = UIDatePicker()
+        self.view.endEditing(true)
+    }
+    
+    @objc func handleCancelButton() {
+        self.view.endEditing(true)
+    }
     
     @IBAction func tappedGetSteps(_ sender: Any) {
-        
+        healthManager.fetchStepCount(dateFromFormat(startDateField.text!), dateFromFormat(endDateField.text!)) { (steps, success) in
+            DispatchQueue.main.async {
+                if success {
+                    print(steps)
+                    self.totalStepLabel.text = String(Int(steps))
+                }
+            }
+        }
     }
     
     
