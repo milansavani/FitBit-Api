6675d5aec03bec3966c3333e2243e8608a01d521
Abhishek Master <abhishekm.mobiiworld@gmail.com>
2019-09-05 12:07:42 +0530

    history vc

---
+++ SampleFitBit/Source/ViewController/HistoryVC.swift
@@ -0,0 +1,39 @@
+//
+//  HistoryVC.swift
+//  SampleFitBit
+//
+//  Created by Mobiiworld on 9/5/19.
+//  Copyright © 2019 mobiiworld. All rights reserved.
+//
+
+import UIKit
+
+class HistoryVC: UIViewController {
+
+    @IBOutlet var startDateButton: UIButton!
+    @IBOutlet var endDateButton: UIButton!
+    
+    @IBOutlet var totalStepLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func tappedStartButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func tappedEndButton(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func tappedGetSteps(_ sender: Any) {
+        
+    }
+    
+    
+
+}
